35e72e48637eb2db2a6a37e742f61967810890a3
Hojjat Ahmadi <maleheabase@gmail.com>
2025-02-24 11:56:49 -0500

    add profile and update register to create automatically profile when user create his/her accounts

--- Accounts/models.py
+++ Accounts/models.py
@@ -2,3 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to='accounts/profile_pics/', default='accounts/default_avatar.jpg')
