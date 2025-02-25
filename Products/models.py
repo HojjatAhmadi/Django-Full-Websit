66f0ea905898b39910a058aa3df0977aa1ef356b
Hojjat Ahmadi <maleheabase@gmail.com>
2025-02-25 08:36:48 -0500

    complete products landding (view and templates)

--- Products/models.py
+++ Products/models.py
@@ -4,11 +4,17 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Products(models.Model):
-    img = models.ImageField(upload_to='products/products_image/', default="products/default_products")
+    img = models.ImageField(upload_to='products/products_image/', default="products/default_products.jpeg")
     name = models.CharField(max_length=100)
     description = models.TextField()
     price_original = models.PositiveIntegerField()
     offer = models.PositiveIntegerField(null=True, blank=True, default=0)
-    price_buy = models.PositiveIntegerField(null=True, blank=True, default=0)
+    price_buy = models.FloatField(null=True, blank=True, default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- Products/views.py
+++ Products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
-from .models import Products
+from .models import Products,Category
 
 
 # Create your views here.
@@ -9,4 +9,8 @@ class ProductsListView(View):
 
     def get(self, request):
         products = Products.objects.all()
-        return render(request, self.template_name, {'products': products})
+        for product in products:
+            if product.price_buy == 0:
+                product.price_buy = product.price_original * (product.offer/100)
+                product.save()
+        return render(request, self.template_name, {'products': products, "category": Category.objects.all()})
