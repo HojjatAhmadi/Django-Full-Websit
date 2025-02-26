2804b6e01e2311428d48b31d0603bc2062e127f3
Hojjat Ahmadi <maleheabase@gmail.com>
2025-02-26 11:27:39 -0500

    create product page with view for see special information

--- Products/urls.py
+++ Products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductsListView
+from .views import ProductsListView, ProductView
 
 app_name = 'products'
 
 urlpatterns = [
     path("", ProductsListView.as_view(), name="products_list"),
+    path("product/<int:pk>", ProductView.as_view(), name="product_view"),
 ]
--- Products/views.py
+++ Products/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View
-from .models import Products,Category
+from .models import Products, Category
 
 
 # Create your views here.
@@ -11,6 +11,14 @@ class ProductsListView(View):
         products = Products.objects.all()
         for product in products:
             if product.price_buy == 0:
-                product.price_buy = product.price_original * (product.offer/100)
+                product.price_buy = product.price_original * (product.offer / 100)
                 product.save()
         return render(request, self.template_name, {'products': products, "category": Category.objects.all()})
+
+
+class ProductView(View):
+    template_name = 'Products/product.html'
+
+    def get(self, request, pk):
+        product = get_object_or_404(Products, pk=pk)
+        return render(request, self.template_name, {'product': product})
